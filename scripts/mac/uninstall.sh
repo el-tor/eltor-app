b8e54042a44507c8a072ee398739b13e16569080
eltor <eltordev@proton.me>
2024-10-12 11:29:39 -0500

    fix mac uninstall script 2

--- scripts/mac/uninstall.sh
+++ scripts/mac/uninstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ -f /Applications/Tor\ Browser.app/Contents/MacOS/Tor/tor_orig ]; then
+#if [ -f /Applications/Tor\ Browser.app/Contents/MacOS/Tor/tor_orig ]; then
   mv /Applications/Tor\ Browser.app/Contents/MacOS/Tor/tor /Applications/Tor\ Browser.app/Contents/MacOS/Tor/tor_el
   mv ~/Library/Application\ Support/TorBrowser-Data/Tor/torrc ~/Library/Application\ Support/TorBrowser-Data/Tor/torrc_el
   mv /Applications/Tor\ Browser.app/Contents/MacOS/Tor/tor_orig /Applications/Tor\ Browser.app/Contents/MacOS/Tor/tor
   mv ~/Library/Application\ Support/TorBrowser-Data/Tor/torrc_orig ~/Library/Application\ Support/TorBrowser-Data/Tor/torrc
   echo "Removed El Tor. Tor Browser should be connected to the normal Tor network."
-else
-  echo "Cannot uninstall. File does not exist /Applications/Tor\ Browser.app/Contents/MacOS/Tor/tor_orig"
-fi
+#else
+#  echo "Cannot uninstall. File does not exist /Applications/Tor\ Browser.app/Contents/MacOS/Tor/tor_orig"
+#fi
 
