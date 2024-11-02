4a1f12b51981c3802a7c2b39ca310628a43875ab
eltor <eltordev@proton.me>
2024-11-01 21:00:31 -0500

    tor-fw-helper for upnp

--- src/renderer/screens/Relay.tsx
+++ src/renderer/screens/Relay.tsx
@@ -18,15 +18,19 @@ export const Relay = () => {
         Relay Active: {relayActive}
       </Text>
       <Text>
-        Run this command in the terminal to start the Tor Relay:
-
-        <CopyableTextBox text='/bin/bash -c "$(curl -fsSL https://bitbucket.org/eltordev/eltor-app/raw/master/scripts/mac/relay.sh)"' />
-
+        1. Run this command in the terminal to start the Tor Relay:
       </Text>
+      <CopyableTextBox text='/bin/bash -c "$(curl -fsSL https://bitbucket.org/eltordev/eltor-app/raw/master/scripts/mac/relay.sh)"' />
       <Text>
-        Run this command to monitor the relay:
-        <CopyableTextBox text="nyx -i 127.0.0.1:8061" />
+        2. Monitor your relay: <br/>(you might need to change the control port 8061 based on your torrc config):
       </Text>
+      <CopyableTextBox text="nyx -i 127.0.0.1:8061" />
+      <Text>
+        3. Firewall: Make sure to open the OrPort on your router. <br/>
+        Or if your router supports UPnP, you can use the following tool to open the port 
+        <a href="https://github.com/Yawning/tor-fw-helper" target="_blank">tor-fw-helper</a>:
+      </Text>
+      <CopyableTextBox text='./tor-fw-helper -p 5061:5061' />
     </Stack>
   );
 };
